618075ec673df47a931907384aad8d6c7a735f4f
superkookai <weerawut@live.com>
2025-05-03 14:17:43 +0700

    Initial Commit

---
+++ DebounceDemo/DebounceDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebounceDemoApp.swift
+//  DebounceDemo
+//
+//  Created by Weerawut Chaiyasomboon on 03/05/2568.
+//
+
+import SwiftUI
+
+@main
+struct DebounceDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
